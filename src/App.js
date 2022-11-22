f086ff16ff018bb78e1e486cff3b43388111e327
gonfdez <gonefdez@gmail.com>
2022-11-22 14:37:56 +0100

    TSX Update

--- src/App.js
+++
@@ -1,9 +0,0 @@
-
-
-function App() {
-  return (
-    <h1>GFS Docs</h1>
-  );
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AppRouter from "./components/router/router";
+
+const App = () : JSX.Element => {
+  return (
+    <AppRouter/>    
+  );
+}
+
+export default App;
