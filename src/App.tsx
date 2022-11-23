66cac48618c9a8d8eab344aee29e2b8c3fa809e4
gonfdez <gonefdez@gmail.com>
2022-11-23 14:34:07 +0100

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import AppRouter from "./components/router/router";
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () : JSX.Element => {
 
 
-  return (
-
-      <AppRouter/>    
-
-  );
+  return (<>
+    <AppRouter/>    
+    <Analytics/>
+  </>);
 }
 
 export default App;
