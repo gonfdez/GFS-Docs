eddf0550027be3bfccdf8bca9967e6074bf8c9e9
gonfdez <gonefdez@gmail.com>
2022-11-24 12:59:46 +0100

    Update home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -8,17 +8,56 @@ const Home = () : JSX.Element => {
   /* Falta SEO */
 
   return (
-    <section className="container-fluid home-container pt-5">
+    <section className="container home-container pt-5">
       <div className="row justify-content-center">
-        
-          <div className="col text-center home-card">
-            <Link to={ALL_CATEGORIES[0].path} className="no-hover">
-              <img src={require('../../media/react.svg').default} alt="React Logo" height={100}/>
-              <h2>
+        <div className="col-12 col-md-auto">
+
+          <div className="row">
+            <Link to={ALL_CATEGORIES[0].path} className="no-hover home-card">
+              <img src={require('../../media/react.svg').default} alt="React Logo" height={100} className="me-5"/>
+              <h2 className="d-inline-block">
                 ReactJS
               </h2>
             </Link>
           </div>
+          
+          <div className="row">
+            <Link to={'/'} className="no-hover home-card">
+              <img src={require('../../media/python.svg').default} alt="Python Logo" height={100} className="me-5"/>
+              <h2 className="d-inline-block">
+                Python
+              </h2>
+            </Link>
+          </div>
+
+          <div className="row">
+            <Link to={'/'} className="no-hover home-card">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/MicroPython_new_logo.svg" alt="Micro Python Logo" height={100} className="me-5"/>
+              <h2 className="d-inline-block">
+                MicroPython
+              </h2>
+            </Link>
+          </div>
+
+          <div className="row">
+            <Link to={'/'} className="no-hover home-card">
+              <img src={require('../../media/mongodb.svg').default} alt="Micro Python Logo" height={100} className="me-5"/>
+              <h2 className="d-inline-block">
+                MongoDB
+              </h2>
+            </Link>
+          </div>
+
+          <div className="row">
+            <Link to={'/'} className="no-hover home-card">
+              <img src={require('../../media/php.svg').default} alt="Micro Python Logo" height={100} className="me-5"/>
+              <h2 className="d-inline-block">
+                PHP
+              </h2>
+            </Link>
+          </div>
+          
+        </div>
       </div>
     </section>
   );
