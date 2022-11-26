136db42881be8e8f08f65b39468a209f0fc0db04
gonfdez <gonefdez@gmail.com>
2022-11-26 22:08:54 +0100

    Fix

--- src/components/posts/postPage.tsx
+++ src/components/posts/postPage.tsx
@@ -42,7 +42,7 @@ const Post = (props : PostPageProps) : JSX.Element => {
       </div>
       <div className="row justify-content-center">
         <div className="col text-center timeline-card">
-            <img src={require('../../media/react.svg').default} alt="React Logo" height={100} />
+            <img src={require('./../../media/react.svg').default} alt="React Logo" height={100} />
             <h2>
               {props.post.emoji}   {props.post.title}
             </h2>
